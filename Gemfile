27e8fc391999e716faf85324baea69d3ce1031b6
Mike Mackintosh <m@zyp.io>
2015-01-24 20:40:31 -0500

    adding in the inital commit

--- Gemfile
+++ Gemfile
@@ -2,8 +2,8 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'ffi-pcap'
-gem 'ffi-packets'
-gem 'ipaddress'
+gem 'ffi-packets', :git => 'https://github.com/Traz/ffi-packets'
+gem 'ipaddress', :git => 'https://github.com/mikemackintosh/ipaddress', :branch => 'adding-#to_h'
 gem 'hexdump'
 
 gem 'pry'
---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'ffi/pcap'
+require 'ffi/packets'
+require 'ipaddress'
+require 'hexdump'
+
+
+pcap =
+  FFI::PCap::Live.new(:dev => 'en0',
+                      :timeout => 1,
+                      :promisc => true,
+                      :handler => FFI::PCap::Handler)
+
+pcap.setfilter("tcp")
+
+pcap.loop(count: 20){|t,p| 
+    ip = FFI::Packets::Ip::Hdr.new raw: p.body
+    tcp = FFI::Packets::TCP::Hdr.new raw: p.body
+    puts "#{ip.src} --> #{ip.dst}"
+    puts tcp.inspect
+    Hexdump.dump(p.body); puts "\n"
+}
+
